95ad0922f02920e9ec3061d08bb444e7ca47fbe3
rodionov <rodionov.mikhail.a@gmail.com>
2019-03-13 16:19:45 +0400

    detect local coordinate. add permission to manifest

--- app/src/main/java/com/rodionov/oktan/MainActivity.kt
+++ app/src/main/java/com/rodionov/oktan/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.rodionov.oktan
 
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.LocationListener
+import android.location.LocationManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.content.ContextCompat
+import android.util.Log
 import com.yandex.mapkit.Animation
 import com.yandex.mapkit.MapKitFactory
 import com.yandex.mapkit.geometry.Point
 import com.yandex.mapkit.map.CameraPosition
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.jar.Manifest
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,8 +22,38 @@ class MainActivity : AppCompatActivity() {
         MapKitFactory.setApiKey("58689044-9fe3-4559-a9f3-1052dac38fb8")
         MapKitFactory.initialize(this)
         setContentView(R.layout.activity_main)
+        val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        if (ContextCompat.checkSelfPermission(applicationContext, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Log.d("myLocation", "request permission")
+            val permissions = arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION)
+            requestPermissions(permissions, 10)
+        } else {
+            Log.d("myLocation", "always PERMISSION_GRANTED")
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 1F, MyLocationListener(coordinateText))
+            val location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+            var longitude = location.longitude
+            var latitude = location.latitude
+            mapView.map.move(CameraPosition(Point(latitude, longitude), 18.0F, 0.0F, 0.0F), Animation(Animation.Type.SMOOTH, 5F), null)
+        }
 
-        mapView.map.move(CameraPosition(Point(55.751574, 37.573856),11.0F, 0.0F, 0.0F), Animation(Animation.Type.SMOOTH, 0F), null)
+//        val location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+//        var longitude = location.longitude
+//        var latitude = location.latitude
+
+//        mapView.map.move(CameraPosition(Point(55.751574, 37.573856), 11.0F, 0.0F, 0.0F), Animation(Animation.Type.SMOOTH, 0F), null)
+//        mapView.map.move(CameraPosition(Point(latitude, longitude),11.0F, 0.0F, 0.0F), Animation(Animation.Type.SMOOTH, 0F), null)
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        Log.d("myLocation", "onRequestPermissionsResult")
+        if (requestCode == 10) {
+            if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Log.d("myLocation", "onRequestPermissionsResult PERMISSION_GRANTED")
+                val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 1F, MyLocationListener(coordinateText))
+            }
+        }
     }
 
     override fun onStop() {
---
+++ app/src/main/java/com/rodionov/oktan/MyLocationListener.kt
@@ -0,0 +1,28 @@
+package com.rodionov.oktan
+
+import android.location.Location
+import android.location.LocationListener
+import android.os.Bundle
+import android.util.Log
+import android.widget.EditText
+import android.widget.TextView
+
+/**
+ * Created by rodionov on 13.03.2019.
+ */
+class MyLocationListener(val text: TextView): LocationListener {
+
+    override fun onLocationChanged(location: Location?) {
+        Log.d("myLocation", "onLocationChanged ${"" + location?.latitude + " " + location?.longitude}")
+        text.text = "" + location?.latitude + " " + location?.longitude
+    }
+
+    override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {
+    }
+
+    override fun onProviderEnabled(provider: String?) {
+    }
+
+    override fun onProviderDisabled(provider: String?) {
+    }
+}
