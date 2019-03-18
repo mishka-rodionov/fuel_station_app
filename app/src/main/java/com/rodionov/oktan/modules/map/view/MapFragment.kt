bffcf165269ea5bbeab9208ea073133f597323c0
Mikhail Rodionov <rodionov.mikhail.a@gmail.com>
2019-03-18 23:37:23 +0400

    change MapFragment.kt

--- app/src/main/java/com/rodionov/oktan/modules/map/view/MapFragment.kt
+++ app/src/main/java/com/rodionov/oktan/modules/map/view/MapFragment.kt
@@ -1,7 +1,15 @@
 package com.rodionov.oktan.modules.map.view
 
+import android.support.v4.app.Fragment
+import android.util.Log
+
 /**
  * Created by rodionov on 18.03.19.
  */
-class MapFragment {
+class MapFragment: Fragment(){
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("mapView", "MapFragment")
+    }
 }
