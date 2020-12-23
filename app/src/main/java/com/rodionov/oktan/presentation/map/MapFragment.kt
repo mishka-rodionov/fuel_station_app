7486a5d2565d9bc819ba2d89bb53e8a3df04755e
mishka_rodionov <rodionov.mikhail.a@gmail.com>
2020-12-24 00:00:48 +0400

    set camera position

--- app/src/main/java/com/rodionov/oktan/presentation/map/MapFragment.kt
+++ app/src/main/java/com/rodionov/oktan/presentation/map/MapFragment.kt
@@ -3,12 +3,12 @@ package com.rodionov.oktan.presentation.map
 import android.os.Bundle
 import android.util.Log
 import com.mapbox.mapboxsdk.Mapbox
+import com.mapbox.mapboxsdk.camera.CameraPosition
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory
 import com.mapbox.mapboxsdk.geometry.LatLng
-import com.mapbox.mapboxsdk.maps.MapboxMap
 import com.mapbox.mapboxsdk.maps.Style
 import com.rodionov.oktan.R
 import com.rodionov.oktan.app.platform.BaseFragment
-import com.rodionov.oktan.app.platform.BaseViewModel
 import com.rodionov.oktan.data.entities.model.Coordinates
 import com.rodionov.oktan.data.entities.model.GasolineStation
 import com.rodionov.oktan.presentation.common.CreateFuelStationDialog
@@ -43,6 +43,7 @@ class MapFragment : BaseFragment(R.layout.fragment_map) {
                 CreateFuelStationDialog(Coordinates(latitude = point.latitude, longitude = point.longitude), ::handleCreateGasolineStation).show(childFragmentManager, "123")
                 true
             }
+            mapboxMap.animateCamera(CameraUpdateFactory.newCameraPosition(setCameraPosition()), 2000)
 
         }
 
@@ -52,5 +53,11 @@ class MapFragment : BaseFragment(R.layout.fragment_map) {
     private fun handleCreateGasolineStation(gasolineStation: GasolineStation) {
         viewModel.createGasolineStation(gasolineStation)
     }
+
+    private fun setCameraPosition()  = CameraPosition.Builder()
+                .target(LatLng(51.607550, 45.93207520))
+                .zoom(10.0)
+                .tilt(20.0)
+                .build()
 
 }
