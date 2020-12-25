0e62cd919a937a2fcbe0265a4a58c0a752f50b39
Михаил Родионов <rodionov.mikhail.a@gmail.com>
2020-12-25 16:46:55 +0400

    set current device location

--- app/src/main/java/com/rodionov/oktan/presentation/map/MapFragment.kt
+++ app/src/main/java/com/rodionov/oktan/presentation/map/MapFragment.kt
@@ -1,8 +1,11 @@
 package com.rodionov.oktan.presentation.map
 
+import android.Manifest
 import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.util.Log
+import androidx.core.content.ContextCompat
+import com.mapbox.android.core.permissions.PermissionsManager
 import com.mapbox.geojson.Feature
 import com.mapbox.geojson.FeatureCollection
 import com.mapbox.geojson.Point
@@ -10,6 +13,10 @@ import com.mapbox.mapboxsdk.Mapbox
 import com.mapbox.mapboxsdk.camera.CameraPosition
 import com.mapbox.mapboxsdk.camera.CameraUpdateFactory
 import com.mapbox.mapboxsdk.geometry.LatLng
+import com.mapbox.mapboxsdk.location.LocationComponentActivationOptions
+import com.mapbox.mapboxsdk.location.LocationComponentOptions
+import com.mapbox.mapboxsdk.location.modes.CameraMode
+import com.mapbox.mapboxsdk.location.modes.RenderMode
 import com.mapbox.mapboxsdk.maps.MapboxMap
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback
 import com.mapbox.mapboxsdk.maps.Style
@@ -26,8 +33,10 @@ import com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconAllowOverlap
 import com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconIgnorePlacement
 import com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconImage
 import com.mapbox.mapboxsdk.style.sources.GeoJsonSource
+import permissions.dispatcher.NeedsPermission
+import permissions.dispatcher.RuntimePermissions
 
-
+@RuntimePermissions
 class MapFragment : BaseFragment(R.layout.fragment_map) {
 
     private val viewModel: MapViewModel by viewModel()
@@ -49,6 +58,7 @@ class MapFragment : BaseFragment(R.layout.fragment_map) {
 
             this.mapboxMap = mapboxMap
             mapboxMap.setStyle(Style.MAPBOX_STREETS) {
+                enableLocationComponentWithPermissionCheck(it)
 //                mapboxMap.addOnMapClickListener { point ->
 //                    Log.d("LOG_TAG", "onMapClick: latitude = ${point.latitude}, longitude = ${point.longitude}, altitude = ${point.altitude}")
 //                    CreateFuelStationDialog().show(childFragmentManager, "123")
@@ -84,6 +94,7 @@ class MapFragment : BaseFragment(R.layout.fragment_map) {
                                 SymbolLayer(LAYER_ID, SOURCE_ID).withProperties(iconImage(ICON_ID), iconAllowOverlap(true), iconIgnorePlacement(true))
                         )
         ) {
+            enableLocationComponentWithPermissionCheck(it)
 //                mapboxMap.addOnMapClickListener { point ->
 //                    Log.d("LOG_TAG", "onMapClick: latitude = ${point.latitude}, longitude = ${point.longitude}, altitude = ${point.altitude}")
 //                    CreateFuelStationDialog().show(childFragmentManager, "123")
@@ -102,6 +113,42 @@ class MapFragment : BaseFragment(R.layout.fragment_map) {
             .zoom(10.0)
             .tilt(20.0)
             .build()
+
+    @NeedsPermission (Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION)
+    fun enableLocationComponent(loadedMapStyle: Style) {
+        // Check if permissions are enabled and if not request
+//        if (PermissionsManager.areLocationPermissionsGranted(requireContext())) {
+
+            // Create and custom    ize the LocationComponent's options
+            val customLocationComponentOptions = LocationComponentOptions.builder(requireContext())
+                    .trackingGesturesManagement(true)
+                    .accuracyColor(ContextCompat.getColor(requireContext(), R.color.colorPrimary))
+                    .build()
+
+            val locationComponentActivationOptions = LocationComponentActivationOptions.builder(requireContext(), loadedMapStyle)
+                    .locationComponentOptions(customLocationComponentOptions)
+                    .build()
+
+            // Get an instance of the LocationComponent and then adjust its settings
+            mapboxMap.locationComponent.apply {
+
+                // Activate the LocationComponent with options
+                activateLocationComponent(locationComponentActivationOptions)
+
+                // Enable to make the LocationComponent visible
+                isLocationComponentEnabled = true
+
+                // Set the LocationComponent's camera mode
+                cameraMode = CameraMode.TRACKING
+
+                // Set the LocationComponent's render mode
+                renderMode = RenderMode.COMPASS
+            }
+//        } else {
+//            permissionsManager = PermissionsManager(this)
+//            permissionsManager.requestLocationPermissions(this)
+//        }
+    }
 
     companion object {
         const val ICON_ID = "IconID"
