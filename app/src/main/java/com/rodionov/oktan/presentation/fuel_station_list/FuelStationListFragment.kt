b1ca58840699a2cb85df03566bce455d14a8c04c
Михаил Родионов <rodionov.mikhail.a@gmail.com>
2021-01-18 14:00:27 +0400

    added adapter to recycler viewfor fuel station list fragment

--- app/src/main/java/com/rodionov/oktan/presentation/fuel_station_list/FuelStationListFragment.kt
+++ app/src/main/java/com/rodionov/oktan/presentation/fuel_station_list/FuelStationListFragment.kt
@@ -4,17 +4,37 @@ import android.os.Bundle
 import com.rodionov.oktan.R
 import com.rodionov.oktan.app.platform.BaseFragment
 import com.rodionov.oktan.app.platform.BaseViewModel
+import com.rodionov.oktan.presentation.fuel_station_list.adapters.GasolineStationAdapter
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import kotlinx.android.synthetic.main.fragment_fuel_stations.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class FuelStationListFragment: BaseFragment(R.layout.fragment_fuel_stations) {
+class FuelStationListFragment : BaseFragment(R.layout.fragment_fuel_stations) {
 
     private val viewModel: FuelStationListViewModel by viewModel()
 
     override val screenViewModel by lazy { viewModel }
 
-    override fun initViews(savedInstanceState: Bundle?) {
-        viewModel.getGasolineStations()
+    private val gasolineStationAdapter by lazy {
+        GasolineStationAdapter()
+    }
 
+    private val mDisposable = CompositeDisposable()
+
+
+    override fun initViews(savedInstanceState: Bundle?) {
+        rvFuelStations.adapter = gasolineStationAdapter
+        mDisposable.add(
+                viewModel.getGasolineStations().subscribe {
+                    gasolineStationAdapter.submitData(lifecycle, it)
+                }
+        )
+
+    }
+
+    override fun onDestroyView() {
+        mDisposable.dispose()
+        super.onDestroyView()
     }
 
 }
