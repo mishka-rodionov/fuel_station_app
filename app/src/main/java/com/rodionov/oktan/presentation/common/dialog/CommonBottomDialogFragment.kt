0d989ce215189878977b804bfbcd963745598bae
Михаил Родионов <rodionov.mikhail.a@gmail.com>
2021-01-24 23:48:54 +0400

    added action bar functionality, added icons for gas gasoline and charging stations, created template for bottom sheet dialog fragment

---
+++ app/src/main/java/com/rodionov/oktan/presentation/common/dialog/CommonBottomDialogFragment.kt
@@ -0,0 +1,64 @@
+package com.rodionov.oktan.presentation.common.dialog
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.os.bundleOf
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.hannesdorfmann.adapterdelegates4.ListDelegationAdapter
+import com.rodionov.oktan.R
+import com.rodionov.oktan.presentation.common.delegates.ItemDialog
+import com.rodionov.oktan.presentation.common.delegates.bottomDialogDelegate
+import kotlinx.android.synthetic.main.dialog_bottom_sheet.*
+
+class CommonBottomDialogFragment : BottomSheetDialogFragment() {
+
+    private var listener: BottomSheetDialogListener? = null
+
+    private val bottomDialogAdapter by lazy {
+        ListDelegationAdapter(
+            bottomDialogDelegate(clickListener)
+        )
+    }
+
+    private val clickListener: (ItemDialog) -> Unit = {
+        listener?.onItemDialogClick(it)
+        dismiss()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = LayoutInflater.from(context).inflate(R.layout.dialog_bottom_sheet, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        listener = targetFragment as? BottomSheetDialogListener
+        val items = arguments?.getSerializable(ARG_ITEMS) as? List<ItemDialog>
+        val title = arguments?.getString(ARG_TITLE)
+        tvTitleBottomSheet.text = title
+        rvBottomSheetDialog.layoutManager = LinearLayoutManager(activity)
+        rvBottomSheetDialog.adapter = bottomDialogAdapter
+
+        bottomDialogAdapter.items = items
+    }
+
+    interface BottomSheetDialogListener {
+        fun onItemDialogClick(itemDialog: ItemDialog)
+    }
+
+    companion object {
+        private const val ARG_ITEMS = "items"
+        private const val ARG_TITLE = "title"
+
+        fun newInstance(items: List<ItemDialog>, title: String = "") =
+            CommonBottomDialogFragment().apply {
+                arguments = bundleOf(
+                    ARG_ITEMS to items,
+                    ARG_TITLE to title
+                    )
+            }
+    }
+}
--- app/src/main/java/com/rodionov/oktan/presentation/main/MainActivity.kt
+++ app/src/main/java/com/rodionov/oktan/presentation/main/MainActivity.kt
@@ -3,6 +3,7 @@ package com.rodionov.oktan.presentation.main
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
@@ -25,6 +26,10 @@ class MainActivity : BaseActivity(R.layout.activity_main), MapboxMap.OnMapClickL
 
     private lateinit var searchManager: SearchManager
     private lateinit var searchSession: Session
+    lateinit var appBarConfiguration: AppBarConfiguration
+    lateinit var host: NavHostFragment
+    lateinit var navController: NavController
+
 
     override fun onMapClick(point: LatLng): Boolean {
         Log.d("LOG_TAG", "onMapClick: latitude = ${point.latitude}, longitude = ${point.longitude}, altitude = ${point.altitude}")
@@ -32,11 +37,19 @@ class MainActivity : BaseActivity(R.layout.activity_main), MapboxMap.OnMapClickL
     }
 
     override fun initViews() {
-
+        host = supportFragmentManager
+                .findFragmentById(R.id.mainFragmentContainer) as NavHostFragment? ?: return
+        navController = host.navController
+        setupBottomNavMenu(navController = navController)
+        appBarConfiguration =
+                AppBarConfiguration(navController.graph, drawerLayout = null)
+        setupActionBar(navController, appBarConfiguration)
     }
 
     override fun initToolbar() {
-
+        setSupportActionBar(toolbarMain as Toolbar)
+        supportActionBar?.title = ""
+        supportActionBar?.setDisplayShowTitleEnabled(false)
     }
 
     private fun setupBottomNavMenu(navController: NavController) {
@@ -49,127 +62,16 @@ class MainActivity : BaseActivity(R.layout.activity_main), MapboxMap.OnMapClickL
         super.onCreate(savedInstanceState)
         Log.d("LOG_TAG", "onCreate: ")
 
-        val host: NavHostFragment = supportFragmentManager
-                .findFragmentById(R.id.mainFragmentContainer) as NavHostFragment? ?: return
-
-        // Set up Action Bar
-        val navController = host.navController
-        setupBottomNavMenu(navController)
-//        Mapbox.getInstance(this, getString(R.string.mapbox_access_token))
+//        val host: NavHostFragment = supportFragmentManager
+//                .findFragmentById(R.id.mainFragmentContainer) as NavHostFragment? ?: return
 //
-//        setContentView(R.layout.activity_main)
-//
-////        mapView = findViewById(R.id.mapView)
-//        mapView?.onCreate(savedInstanceState)
-//        mapView?.getMapAsync { mapboxMap ->
-//
-//            mapboxMap.setStyle(Style.MAPBOX_STREETS) {
-//                mapboxMap.addOnMapClickListener( this )
-//
-//// Map is set up and the style has loaded. Now you can add data or make other map adjustments
-//
-//
-//            }
-//        }
-
-
-//        MapKitFactory.setApiKey("58689044-9fe3-4559-a9f3-1052dac38fb8")
-//        MapKitFactory.initialize(this)
-//        SearchFactory.initialize(this)
-//        searchManager = SearchFactory.getInstance().createSearchManager(SearchManagerType.ONLINE)
-//        setContentView(R.layout.activity_main)
-//        val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
-//        if (ContextCompat.checkSelfPermission(applicationContext, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-//            Log.d("myLocation", "request permission")
-//            val permissions = arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION)
-//            requestPermissions(permissions, 10)
-//        } else {
-//            Log.d("myLocation", "always PERMISSION_GRANTED")
-//            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 1F, MyLocationListener(coordinateText))
-//            val location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
-//            var longitude = location!!.longitude
-//            var latitude = location!!.latitude
-//            mapView.map.move(CameraPosition(Point(latitude, longitude), 14.0F, 0.0F, 0.0F), Animation(Animation.Type.LINEAR, 0F), null)
-//        }
-//
-//        submitQuery("АЗС")
-
-
-//        Log.d("myLocation", "${mapView.map.mapObjects.userData.toString()}")
-//        val location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
-//        var longitude = location.longitude
-//        var latitude = location.latitude
-
-//        mapView.map.move(CameraPosition(Point(55.751574, 37.573856), 11.0F, 0.0F, 0.0F), Animation(Animation.Type.SMOOTH, 0F), null)
-//        mapView.map.move(CameraPosition(Point(latitude, longitude),11.0F, 0.0F, 0.0F), Animation(Animation.Type.SMOOTH, 0F), null)
+//        // Set up Action Bar
+//        val navController = host.navController
+//        setupBottomNavMenu(navController)
     }
 
-//    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-//        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-//        Log.d("myLocation", "onRequestPermissionsResult")
-//        if (requestCode == 10) {
-//            if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-//                Log.d("myLocation", "onRequestPermissionsResult PERMISSION_GRANTED")
-//                val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
-//                if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-//                    // TODO: Consider calling
-//                    //    ActivityCompat#requestPermissions
-//                    // here to request the missing permissions, and then overriding
-//                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-//                    //                                          int[] grantResults)
-//                    // to handle the case where the user grants the permission. See the documentation
-//                    // for ActivityCompat#requestPermissions for more details.
-//                    return
-//                }
-//                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 1F, MyLocationListener(coordinateText))
-//            }
-//        }
-//    }
+    override fun onSupportNavigateUp(): Boolean {
+        return findNavController(R.id.mainFragmentContainer).navigateUp(appBarConfiguration)
+    }
 
-//    override fun onResume() {
-//        super.onResume()
-//        mapView.onResume()
-//    }
-//
-//    override fun onStart() {
-//        super.onStart()
-//        mapView.onStart()
-//    }
-//
-//    override fun onStop() {
-//        super.onStop()
-//        mapView.onStop()
-//    }
-//
-//    override fun onPause() {
-//        super.onPause()
-//        mapView.onPause()
-//    }
-//
-//    override fun onLowMemory() {
-//        super.onLowMemory()
-//        mapView.onLowMemory()
-//    }
-//
-//    override fun onDestroy() {
-//        super.onDestroy()
-//        mapView.onDestroy()
-//    }
-//
-//    override fun onSaveInstanceState(outState: Bundle) {
-//        super.onSaveInstanceState(outState)
-//        mapView.onSaveInstanceState(outState)
-//    }
-
-//    override fun onStop() {
-//        super.onStop()
-////        mapView.onStop()
-////        MapKitFactory.getInstance().onStop()
-//    }
-//
-//    override fun onStart() {
-//        super.onStart()
-////        mapView.onStart()
-////        MapKitFactory.getInstance().onStart()
-//    }
 }
