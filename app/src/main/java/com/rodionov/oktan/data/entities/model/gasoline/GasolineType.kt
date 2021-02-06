e78c4c957b70b7cca8964e756060959b010cd465
Михаил Родионов <rodionov.mikhail.a@gmail.com>
2021-02-06 23:52:16 +0400

    chaged constarints layout to motion layout, added style for rounded bottom sheet dialog, changed json field name for gasoline type

--- app/src/main/java/com/rodionov/oktan/data/entities/model/gasoline/GasolineType.kt
+++ app/src/main/java/com/rodionov/oktan/data/entities/model/gasoline/GasolineType.kt
@@ -1,7 +1,12 @@
 package com.rodionov.oktan.data.entities.model.gasoline
 
+import com.google.gson.annotations.SerializedName
+
 data class GasolineType(
+        @SerializedName("name")
         val name: GasolineName? = null,
+        @SerializedName("price_per_liter")
         val pricePerLiter: Float? = null,
+        @SerializedName("real_oktan_number")
         val realOktanNumber: Float? = null
 )
